fa0d978ace66b2ffcc3907dc2ed35f2d48b49372
seba.energias@gmail.com <seba.energias@gmail.com>
2024-04-03 20:48:39 -0300

    se agrega funcionalidad JS en botones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,21 @@
+
+
+$(document).ready(function () {
+  $('#enviarCorreo').click(function () {
+     alert('Se envió correctamente el correo.');
+  });
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   let carousel = document.querySelector('.carousel');
   let intervalId = setInterval(function() {
       $('.carousel').carousel('next');
-  }, 6000); // Cambia la imagen cada 3 segundos
+  }, 8000); // Cambia la imagen cada 8 segundos
 
   
 });
+
+var tooltips = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+var tooltipList = tooltips.map(function (tooltipEl) {
+    return new bootstrap.Tooltip(tooltipEl);
+}); // Para que funcionen los tooltip
