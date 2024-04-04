1e216440da547835dbdc76f12c955a6340a92174
seba.energias@gmail.com <seba.energias@gmail.com>
2024-04-03 21:43:09 -0300

    se agrega funcionalidad en js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,50 @@
-
-
 $(document).ready(function () {
-  $('#enviarCorreo').click(function () {
-     alert('Se envió correctamente el correo.');
-  });
+  $("#enviarCorreo").click(function () {
+    alert("Se envió correctamente el correo.");
+  }); //envia un mensaje de alerta cuando se presiona enviar
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  let carousel = document.querySelector('.carousel');
-  let intervalId = setInterval(function() {
-      $('.carousel').carousel('next');
+  let carousel = document.querySelector(".carousel");
+  let intervalId = setInterval(function () {
+    $(".carousel").carousel("next");
   }, 8000); // Cambia la imagen cada 8 segundos
-
-  
 });
 
-var tooltips = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+$(document).ready(function () {
+  // Agregar desplazamiento suave a todos los enlaces
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate(
+        // desplazamiento suave de la página
+
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        1000, //tiempo que se demora en llegar al hash
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > $("#quienes").offset().top) {
+    //cambia de color a negro cuando pasa la seccion quienes
+    $(".navbar").addClass("bg-black");
+  } else {
+    $(".navbar").removeClass("bg-black ");
+  }
+});
+var tooltips = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
 var tooltipList = tooltips.map(function (tooltipEl) {
-    return new bootstrap.Tooltip(tooltipEl);
-}); // Para que funcionen los tooltip
+  return new bootstrap.Tooltip(tooltipEl);
+}); // Para que funcionen los tooltip y muestra un mensaje al posicionarse sobre el boton
